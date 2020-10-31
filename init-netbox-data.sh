06696f5537281f87b7d910b7bc2013fdc85bfadb
Lars Kellogg-Stedman <lars@oddbit.com>
2020-10-31 17:19:15 -0400

    make netbox-docker git url configurable

--- init-netbox-data.sh
+++ init-netbox-data.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-git clone -b ${NETBOX_GIT_REF:-develop} \
-	https://github.com/netbox-community/netbox-docker.git \
-	/tmp/netbox-docker
+: ${NETBOX_GIT_URL:=https://github.com/netbox-community/netbox-docker.git}
+: ${NETBOX_GIT_REF:=develop}
 
-cd /tmp/netbox-docker
+git clone -b ${NETBOX_GIT_REF} ${NETBOX_GIT_URL} /tmp/netbox-config
+
+cd /tmp/netbox-config
 for dir in startup_scripts initializers configuration reports scripts; do
 	tar -cf- $dir | tar -C /target -xf-
 done
